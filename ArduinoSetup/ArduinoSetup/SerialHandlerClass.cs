38da7c8e5f48cb867b45ed80f767ec6bd720b96f
andrew beaumont <madmagican3@gmail.com>
2017-12-04 23:08:18 +0000

    Continued to get it to work with the char returns

--- ArduinoSetup/ArduinoSetup/SerialHandlerClass.cs
+++ ArduinoSetup/ArduinoSetup/SerialHandlerClass.cs
@@ -18,6 +18,11 @@ namespace ArduinoSetup
         /// </summary>
         private ArduinoControllerForm form;
         /// <summary>
+        /// This is used to indiciate if we should look for a number 
+        /// </summary>
+        private bool gettingWallNumber;
+        
+        /// <summary>
         /// This controller sets up the class so that when we run it as a thread it can act autonomously
         /// </summary>
         public SerialHandlerClass(String port, ArduinoControllerForm form)
@@ -43,7 +48,28 @@ namespace ArduinoSetup
                 if (s != null)
                 {
                     form.SerialReturnsList.Items.Add(s);
+                    IdentifyChar(s);
                 }
+            }
+        }
+        /// <summary>
+        /// This will check to see if the chars are the ones we need to interact with
+        /// </summary>
+        public void IdentifyChar(String localString)
+        {
+            int tempNo;
+            if (localString.Length <= 0)
+            {
+                return;
+            }
+            var charArray = localString.ToCharArray();
+            // if we've encountered a wall
+            if (charArray[0] == 'w')
+            {
+                
+            }else if (gettingWallNumber && int.TryParse(localString,out tempNo))
+            {
+                
             }
         }
     }
